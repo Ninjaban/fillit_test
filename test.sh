2c51628e396efb7e03abb620a1c687f959d91222
Jonathan CARRA <jcarra@e1r3p6.42.fr>
2016-12-23 08:57:45 +0100

    ok

--- test.sh
+++ test.sh
@@ -1,47 +1,44 @@
 echo "START\n"
 echo "ERROR TEST:\n"
-cat error0
-$1 error0
-echo "\n"
-cat error1
-$1 error1
-echo "\n"
-cat error2
-$1 error2
-echo "\n"
-cat error3
-$1 error3
-echo "\n"
-cat error4
-$1 error4
-echo "\n"
-cat error5
-$1 error5
-echo "\n"
-cat error6
-$1 error6
-echo "\n"
-cat error7
-$1 error7
-echo "\n"
+cat error0 | less
+$1 error0 | less
+
+cat error1 | less
+$1 error1 | less
+
+cat error2 | less
+$1 error2 | less
+
+cat error3 | less
+$1 error3 | less
+
+cat error4 | less
+$1 error4 | less
+
+cat error5 | less
+$1 error5 | less
+
+cat error6 | less
+$1 error6 | less
+
+cat error7 | less
+$1 error7 | less
 echo "BASIC TEST:\n"
-cat file0
-$1 file0
-echo "\n"
-cat file1
-$1 file1
-echo "\n"
-cat file2
-$1 file2
-echo "\n"
-cat file3
-$1 file3
-echo "\n"
+cat file0 | less
+$1 file0 | less
+
+cat file1 | less
+$1 file1 | less
+
+cat file2 | less
+$1 file2 | less
+
+cat file3 | less
+$1 file3 | less
 echo "TIME TEST:\n"
-cat file4
-time $1 file4
-echo "\n"
-cat file5
-time $1 file5
-echo "\n"
+cat file4 | less
+time $1 file4 | less
+
+cat file5 | less
+time $1 file5 | less
 echo "END\n"
